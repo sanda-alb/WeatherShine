466376d83ba6521482397d08e8466dd9e21e8a27
albrekht <albrekht@kinoplan.ru>
2022-05-16 20:49:49 +0300

    Initial Commit

---
+++ WeatherShine/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeatherShine
+//
+//  Created by Sanda Albrecht on 5/16/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
