01bcd61c65b607d8d979575557b4a733ad7692e2
albrekht <albrekht@kinoplan.ru>
2022-05-17 18:23:58 +0300

    Deleted storyboard

--- WeatherShine/ViewController.swift
+++ WeatherShine/ViewController.swift
@@ -6,13 +6,17 @@
 //
 
 import UIKit
+import SnapKit
+import CoreLocation
 
 class ViewController: UIViewController {
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
 
 
 }
