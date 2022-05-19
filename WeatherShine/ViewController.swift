510fccb484d7eddb3698c214ce5e30c741c61f4a
albrekht <albrekht@kinoplan.ru>
2022-05-19 21:16:38 +0300

    Got a city from timestamp

--- WeatherShine/ViewController.swift
+++ WeatherShine/ViewController.swift
@@ -11,7 +11,9 @@ import CoreLocation
 
 class MyViewController: UIViewController {
 
-    private let label = UILabel()
+    private let cityLabel = UILabel()
+    private let timeLabel = UILabel()
+    
     private let locationManager = CLLocationManager()
 
     override func viewDidLoad() {
@@ -24,21 +26,30 @@ class MyViewController: UIViewController {
     }
 
     private func embedViews() {
-        view.addSubview(label)
+        view.addSubview(cityLabel)
+        view.addSubview(timeLabel)
+    
     }
 
     private func setupLayout() {
-        label.snp.makeConstraints { make in
+        cityLabel.snp.makeConstraints { make in
             make.width.equalTo(300)
-            make.height.equalTo(250)
+            make.height.equalTo(100)
             make.centerX.equalToSuperview()
             make.centerY.equalToSuperview()
+        }
+        
+        timeLabel.snp.makeConstraints { make in
+            make.height.equalTo(100)
+            make.centerX.equalToSuperview()
+            make.top.equalTo(cityLabel.snp.bottom).offset(50)
         }
     }
 
     private func setupAppearance() {
         view.backgroundColor = .white
-        label.numberOfLines = 2
+        cityLabel.numberOfLines = 2
+        cityLabel.textAlignment = .center
     }
 
     private func setupBehaviour() {
@@ -63,6 +74,17 @@ extension MyViewController: CLLocationManagerDelegate {
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
       let newLocation = locations.last!
       print("didUpdateLocations \(newLocation)")
-        label.text = "latitude: \(newLocation.coordinate.latitude), longitude: \(newLocation.coordinate.longitude)"
+        let time = newLocation.timestamp
+        timeLabel.text = "\(time)"
+        
+        
+       
+        let geocoder = CLGeocoder()
+        geocoder.reverseGeocodeLocation(newLocation) { placemarks, _ in
+                if let place = placemarks?.first {
+                    print("place: \(place.administrativeArea)")
+                    self.cityLabel.text = place.administrativeArea
+                }
+        }
     }
 }
